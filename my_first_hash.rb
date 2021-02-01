749d6aaf297669ef2a7b133bed24c8f2fc4dccc6
Milan Mody <milanm1120@gmail.com>
2021-02-01 22:47:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,9 +25,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  return shipping_manifest["oil paintings"]
+  
 end
-
+return shipping_manifest["oil paintings"]
 
 def adding
   shipping_manifest = {
