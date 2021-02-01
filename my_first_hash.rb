5b64e3c66ced44ebbcbe95dc7779c57a71c4a996
Milan Mody <milanm1120@gmail.com>
2021-02-01 22:49:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 the_manifest = {"whale bone corsets" => 5,
                 "porcelain vases" => 2,
-                "oil paintings" => 3
+                "oil paintings" => 3,
 }
 end
 
@@ -21,7 +21,7 @@ def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
                  }
         shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
