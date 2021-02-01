e9c243858a8d5f02b89072ab98fec5f2de65aab1
Milan Mody <milanm1120@gmail.com>
2021-02-01 22:34:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,9 +17,9 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale_bone_corsets" => 5,
-                  "porcelain_vases" => 2,
-                  "oil_paintings" => 3
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
